3797de8a8a242b32eac9b40c94f915f8231a74f3
aaron <aaron@moduparking.com>
2023-10-23 23:34:34 +0900

    chore: 불필요 로직 제거

--- src/app/api/guestbook/route.ts
+++ src/app/api/guestbook/route.ts
@@ -1,17 +1,7 @@
 import { NextResponse } from "next/server";
 import { prisma } from "@/app/db";
 
-export async function main() {
-  try {
-    await prisma.$connect();
-    console.log("connect");
-  } catch {
-    return Error("DB Connection Error");
-  }
-}
-
 export const GET = async () => {
-  await main();
   try {
     const data = await prisma.guestbook.findMany();
     return NextResponse.json({ message: "Success", data }, { status: 200 });
