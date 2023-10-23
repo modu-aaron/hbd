edbc27c1c8c43e9bc72fd75d2f515b8c8cedc63e
aaron <aaron@moduparking.com>
2023-10-23 23:22:27 +0900

    chore: connect 로직 추가

--- src/app/api/guestbook/route.ts
+++ src/app/api/guestbook/route.ts
@@ -1,15 +1,16 @@
 import { NextResponse } from "next/server";
 import { prisma } from "@/app/db";
 
-// export async function main() {
-//   try {
-//     await prisma.$connect();
-//   } catch {
-//     return Error("DB Connection Error");
-//   }
-// }
+export async function main() {
+  try {
+    await prisma.$connect();
+  } catch {
+    return Error("DB Connection Error");
+  }
+}
 
 export const GET = async (req: Request, res: NextResponse) => {
+  await main();
   try {
     const data = await prisma.guestbook.findMany();
     return NextResponse.json({ message: "Success", data }, { status: 200 });
