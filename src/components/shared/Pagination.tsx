480018bd17eb758a54690ee3acf1b22991f8feaf
aaron <aaron@moduparking.com>
2023-10-23 19:56:34 +0900

    fix: pagination 버그 수정 및 레이아웃 수정

--- src/components/shared/Pagination.tsx
+++ src/components/shared/Pagination.tsx
@@ -14,6 +14,8 @@ const PaginationView = ({
   total,
 }: PaginationProps) => {
   const totalPages = Math.ceil(total / pageSize);
+  const firstNum = Math.floor((current - 1) / 5) * 5 + 1;
+  const lastNum = Math.min(firstNum + 4, totalPages);
 
   const handleClick = (num: number) => {
     if (onChange) {
@@ -21,58 +23,21 @@ const PaginationView = ({
     }
   };
 
-  // 페이지 버튼을 동적으로 생성
   const pageButtons = [];
 
-  for (let i = 1; i <= totalPages; i++) {
-    // 첫 5개 페이지는 그대로 출력
-    if (i <= 4) {
-      pageButtons.push(
-        <li key={i}>
-          <button
-            className={`flex items-center justify-center w-6 h-6 mr-1 rounded-full dark:bg-neutral-300 bg-sky-300/30 leading-tight ${
-              current === i ? "text-blue-600" : "text-gray-500"
-            }`}
-            onClick={() => handleClick(i)}
-          >
-            {i}
-          </button>
-        </li>
-      );
-    }
-    // 마지막 페이지는 출력
-    else if (i === totalPages) {
-      pageButtons.push(
-        <li key={i}>
-          <button
-            className={`flex items-center gap-3 justify-center px-2 h-6 rounded-full dark:bg-neutral-300 bg-sky-300/30 leading-tight ${
-              current === i ? "text-blue-600" : "text-gray-500"
-            }`}
-            onClick={() => handleClick(i)}
-          >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              strokeWidth={1.5}
-              stroke={`${
-                current < totalPages && current > 4
-                  ? "rgb(37 99 235)"
-                  : "rgb(107 114 128)"
-              }`}
-              className="w-6 h-6"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M6.75 12a.75.75 0 11-1.5 0 .75.75 0 011.5 0zM12.75 12a.75.75 0 11-1.5 0 .75.75 0 011.5 0zM18.75 12a.75.75 0 11-1.5 0 .75.75 0 011.5 0z"
-              />
-            </svg>
-            {totalPages}
-          </button>
-        </li>
-      );
-    }
+  for (let i = firstNum; i <= lastNum; i++) {
+    pageButtons.push(
+      <li key={i}>
+        <button
+          className={`flex items-center justify-center w-6 h-6 mr-1 rounded-full dark:bg-neutral-300 bg-sky-300/30 leading-tight ${
+            current === i ? "text-blue-600" : "text-gray-500"
+          }`}
+          onClick={() => handleClick(i)}
+        >
+          {i}
+        </button>
+      </li>
+    );
   }
 
   return (
