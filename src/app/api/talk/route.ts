d24e09588aaf967507d2a6921d99b57fdd7dbb54
aaron <aaron@moduparking.com>
2023-10-24 11:16:35 +0900

    chore: runtime 설정 제거

--- src/app/api/talk/route.ts
+++ src/app/api/talk/route.ts
@@ -2,13 +2,9 @@ import { OpenAIStream, StreamingTextResponse } from "ai";
 import OpenAI from "openai";
 import prisma from "@/app/db";
 
-// Create an OpenAI API client (that's edge friendly!)
 const openai = new OpenAI({
   apiKey: `${process.env.OPENAI_API_KEY}`,
 });
-
-// IMPORTANT! Set the runtime to edge
-export const runtime = "edge";
 
 export async function POST(req: Request) {
   const { messages } = await req.json();
