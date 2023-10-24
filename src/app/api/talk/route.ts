b91e4f143bbb3dfb848014312454b64525272364
aaron <aaron@moduparking.com>
2023-10-24 11:32:09 +0900

    refactor: openai로 전환

--- src/app/api/talk/route.ts
+++ src/app/api/talk/route.ts
@@ -1,17 +1,15 @@
 import { OpenAIStream, StreamingTextResponse } from "ai";
-import { Configuration, OpenAIApi } from "openai-edge";
 import prisma from "@/app/db";
+import OpenAI from "openai";
 
-const config = new Configuration({
+const openai = new OpenAI({
   apiKey: `${process.env.OPENAI_API_KEY}`,
 });
-
-const openai = new OpenAIApi(config);
 
 export async function POST(req: Request) {
   const { messages } = await req.json();
 
-  const response = await openai.createChatCompletion({
+  const response = await openai.chat.completions.create({
     model: "gpt-3.5-turbo",
     stream: true,
     messages: messages,
