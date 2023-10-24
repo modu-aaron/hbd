190fc8334b0de558ba935636f88ededc4f2a4eb1
aaron <aaron@moduparking.com>
2023-10-24 11:07:17 +0900

    chore: env 설정 수정

--- src/app/api/talk/route.ts
+++ src/app/api/talk/route.ts
@@ -3,7 +3,7 @@ import OpenAI from "openai";
 import prisma from "@/app/db";
 
 const openai = new OpenAI({
-  apiKey: process.env.OPENAI_API_KEY!,
+  apiKey: "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",
 });
 
 export async function POST(req: Request) {
