98d8f6351904037cb1d3ebefb3346bc93c108d5c
aaron <aaron@moduparking.com>
2023-10-24 11:00:51 +0900

    refactor: openai-edge -> openai 로 전환

--- src/app/api/talk/route.ts
+++ src/app/api/talk/route.ts
@@ -1,23 +1,20 @@
 import { OpenAIStream, StreamingTextResponse } from "ai";
-import { Configuration, OpenAIApi } from "openai-edge";
+import OpenAI from "openai";
 import prisma from "@/app/db";
 
-const config = new Configuration({
+const openai = new OpenAI({
   apiKey: process.env.OPENAI_API_KEY,
 });
-
-const openai = new OpenAIApi(config);
 
 export async function POST(req: Request) {
   const { messages } = await req.json();
 
-  const response = await openai.createChatCompletion({
+  const response = await openai.chat.completions.create({
     model: "gpt-3.5-turbo",
     stream: true,
     messages: messages,
   });
 
-  console.log("도달");
   const stream = OpenAIStream(response, {
     onCompletion: async (completion: string) => {
       const data = await prisma.message.create({
