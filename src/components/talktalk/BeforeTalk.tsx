b2d51fa8c1d45bbcfceec4cf8a13b1e1f8309c7d
aaron <aaron@moduparking.com>
2023-10-25 15:59:32 +0900

    refactor: FinnIcon 추가 및 적용

--- src/components/talktalk/BeforeTalk.tsx
+++ src/components/talktalk/BeforeTalk.tsx
@@ -4,9 +4,12 @@ import Icon from "@/components/Icons/Icon";
 const BeforeTalk = () => {
   return (
     <div className="w-full flex flex-col items-center gap-2 justify-center pt-8">
-      <h1 className="font-bold flex gap-1 items-center text-center text-2xl text-gray-900 dark:text-white">
-        {Icon.JakeCloudIcon} 궁금한걸 질문해봐요
-      </h1>
+      <div className="flex gap-1 items-center">
+        <h1 className="font-bold text-center text-2xl text-gray-900 dark:text-white">
+          궁금한걸 질문해봐요
+        </h1>
+        {Icon.FinnIcon}
+      </div>
       <span className="flex items-center">
         생일이라 바쁜 다롱이{Icon.JakeIcon} 대신 알려줄게요
       </span>
