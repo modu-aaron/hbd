9fcd3a80579c6b5ebfcf536394abe45dcf81b2b3
aaron <aaron@moduparking.com>
2023-10-26 00:47:09 +0900

    feat: test 로직 추가

--- src/app/api/guestbook/route.ts
+++ src/app/api/guestbook/route.ts
@@ -1,39 +1,39 @@
-import { NextResponse } from "next/server";
-import prisma from "@/app/db";
+// import { NextResponse } from "next/server";
+// import prisma from "@/app/db";
 
-export const GET = async () => {
-  try {
-    const data = await prisma.guestbook.findMany({
-      take: 200,
-      orderBy: {
-        created_at: "desc",
-      },
-    });
-    return NextResponse.json({ message: "Success", data }, { status: 200 });
-  } catch (err) {
-    return NextResponse.json({ message: "Error is occured" }, { status: 500 });
-  }
-};
+// export const GET = async () => {
+//   try {
+//     const data = await prisma.guestbook.findMany({
+//       take: 200,
+//       orderBy: {
+//         created_at: "desc",
+//       },
+//     });
+//     return NextResponse.json({ message: "Success", data }, { status: 200 });
+//   } catch (err) {
+//     return NextResponse.json({ message: "Error is occured" }, { status: 500 });
+//   }
+// };
 
-export async function DELETE(req: Request) {
-  try {
-    const { id } = await req.json();
-    const entry = await prisma.guestbook.delete({ where: { id } });
-    return NextResponse.json({ message: "Success", entry }, { status: 200 });
-  } catch (error) {
-    return NextResponse.json({ message: "Error is occured" }, { status: 500 });
-  }
-}
+// export async function DELETE(req: Request) {
+//   try {
+//     const { id } = await req.json();
+//     const entry = await prisma.guestbook.delete({ where: { id } });
+//     return NextResponse.json({ message: "Success", entry }, { status: 200 });
+//   } catch (error) {
+//     return NextResponse.json({ message: "Error is occured" }, { status: 500 });
+//   }
+// }
 
-export async function PUT(req: Request) {
-  try {
-    const { message, id } = await req.json();
-    const entry = await prisma.guestbook.update({
-      data: { message },
-      where: { id },
-    });
-    return NextResponse.json({ message: "Success", entry }, { status: 200 });
-  } catch (error) {
-    return NextResponse.json({ message: "Error is occured" }, { status: 500 });
-  }
-}
+// export async function PUT(req: Request) {
+//   try {
+//     const { message, id } = await req.json();
+//     const entry = await prisma.guestbook.update({
+//       data: { message },
+//       where: { id },
+//     });
+//     return NextResponse.json({ message: "Success", entry }, { status: 200 });
+//   } catch (error) {
+//     return NextResponse.json({ message: "Error is occured" }, { status: 500 });
+//   }
+// }
